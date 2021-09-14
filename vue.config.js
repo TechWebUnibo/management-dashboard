c4f02ad44970d25f6164730fb16d2cbd241375e9
elements72 <antonio.lopez.8523@gmail.com>
2021-09-14 22:26:35 +0200

    Modified scp command

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   outputDir: path.resolve(__dirname,
     "dist"),
-  indexPath: 'html/index.html',
+  indexPath: 'html/management-dashboard/index.html',
   publicPath: '/public',
   css: {
     extract: {
