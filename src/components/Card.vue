3d5d15a55b6a5d734e651abbca67c3529a0cca8d
elements72 <antonio.lopez.8523@gmail.com>
2021-09-17 19:39:41 +0200

    Customer stats anf charts finished

---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <div class="card" >
+        <img class="card-img" :src="require(`@/assets/${card.img}`)" alt="Card image">
+        <div class="card-body">
+            <h5 class="card-title">{{card.title}}</h5>
+            <p class="card-text">{{card.text}}</p>
+            <router-link :to="card.link" class="btn btn-primary">Show</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props:{
+        card: Object
+    },
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+    .card-text{
+        color: black;
+    }
+    .card-title{
+        color: black;
+    }
+    img{
+        height: 70%;
+    }
+</style>
---
+++ src/components/CustomerCard.vue
@@ -0,0 +1,64 @@
+<template>
+
+<div class="card" style="width: 18rem;">
+    <div class="card-header">
+        <img class="card-img-top"  :src="customer.avatar"  alt="Customer avatar">
+    </div>
+  <div class="card-body">
+    <h5 class="card-title">{{customer.username}}</h5>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item active">The customer has made: {{customer.rentInfo.total}} rentals for a total of {{customer.turnover}} euro</li>
+        <li class="list-group-item">{{customer.rentInfo.not_started}} not started</li>
+        <li class="list-group-item">{{customer.rentInfo.in_progress}} in progress</li>
+        <li class="list-group-item">{{customer.rentInfo.terminated}} terminated</li>
+        <li class="list-group-item">{{customer.rentInfo.cancelled}} cancelled</li>
+        <li class="list-group-item">{{customer.rentInfo.delayed}} delayed</li>
+    </ul>
+  </div>
+  <ul class="list-group list-group-flush" v-if="customer.address">
+    <li class="list-group-item">Lives in: {{customer.address.city }}</li>
+    <li class="list-group-item">Zip code: {{customer.address.zip }}</li>
+    <li class="list-group-item">Residence: {{customer.address.residence }}</li>
+  </ul>
+  <ul class="list-group list-group-flush" v-if="!customer.address">
+    <li class="list-group-item">No other info</li>
+  </ul>
+</div>
+
+</template>
+
+<script>
+
+export default {
+    name: 'Card',
+    data: function (){
+        return {
+        }
+    },
+    props:{
+        customer: Object
+    },
+    created(){
+        if(typeof this.customer.avatar === 'undefined' || this.customer.avatar === ''){
+            this.customer.avatar = '/img/avatar/generic-user-icon.png'
+        }
+    }
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+    .card-text{
+        color: black;
+    }
+    .card-title{
+        color: black;
+    }
+    .card-header{
+        min-height: 40%;
+        display: flex;
+        align-items: center;
+    }
+
+</style>
